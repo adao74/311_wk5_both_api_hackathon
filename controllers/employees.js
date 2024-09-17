59c69ce6b2a664b731c484794efc49ecd16f3d90
adao74 <andreaminghweidao@hotmail.com>
2024-09-16 21:01:18 -0500

    add index.js & controllers.js file

---
+++ controllers/employees.js
@@ -0,0 +1,13 @@
+const getEmployees = () => {
+  res.send("getting employees...")
+}  
+
+const getEmployeesById = () => {
+    res.send("getting employees...")
+}
+
+const getEmployeesByFirstName = () => {
+    res.send("getting employees...")
+}
+
+module.exports = { getEmployees, getEmployeesById, getEmployeesByFirstName }
