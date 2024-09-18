1d8b3065035cdf5d5abd93450cf81758832824ce
CyrellEstrada <cyrellestrada1@gmail.com>
2024-09-18 18:58:58 -0500

    Added controllers to user routes, has not been tested

--- routes/employee.js
+++ routes/employee.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const controllers = require("../controllers/employees")
 
-router.get('/', res.send("getting employees"));
-router.get('/:id', res.send("getting employees"));
-router.get('firstname/:first_name', res.send("getting employees"));
+router.get('/', controllers.getEmployees);
+router.get('/:id', controllers.getEmployeesById);
+router.get('firstname/:first_name', controllers.getEmployeesByFirstName);
 
 module.exports = router
