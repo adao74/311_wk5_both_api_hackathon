44662d20be8535832eff5146d375905cfc4a361a
adao74 <andreaminghweidao@hotmail.com>
2024-09-18 18:47:22 -0500

    add controllers (step 4)

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,12 +1,12 @@
-const getEmployees = () => {
+const getEmployees = (req, res) => {
   res.send("getting employees...")
 }  
 
-const getEmployeesById = () => {
+const getEmployeesById = (req, res) => {
     res.send("getting employees...")
 }
 
-const getEmployeesByFirstName = () => {
+const getEmployeesByFirstName = (req, res) => {
     res.send("getting employees...")
 }
 
