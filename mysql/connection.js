bbb66ae63a579ab563d59867b0408c7d48b4d5a2
CyrellEstrada <cyrellestrada1@gmail.com>
2024-09-18 19:37:07 -0500

    Created connection pool in mysql

---
+++ mysql/connection.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql')
+
+const singleton = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+})
+
+// const singleton = () => {
+//     if (!this.pool) {
+//         console.log('creating connection...')
+//         this.pool = mysql.createPool({
+//             connectionLimit: 100,
+//             host: process.env.HOST,
+//             user: process.env.USER,
+//             password: process.env.PASSWORD,
+//             database: process.env.DATABASE
+//         })
+
+//         return this.pool
+//     }
+//     return this.pool
+// }
+
+module.exports = singleton
